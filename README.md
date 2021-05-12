2586d2a74e935140c3c2df77b7fd67e855677e90
김용건 (Kim Younggeon) <mail@kyg.kr>
2021-05-12 13:49:47 +0900

    chore: nuxt 추가

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Yarn Berry + Nuxt.js
 프로젝트 적용 전 검증 레파지토리
+
+## 발생한 예외
+1. consola를 따로 설치해줘야함
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import type { NuxtOptions } from '@nuxt/types'
+
+const nuxtOptions: Partial<NuxtOptions> = {
+  buildModules: ['@nuxtjs/typescript-build']
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default nuxtOptions
